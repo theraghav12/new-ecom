bc2edb75deeb0e660ecfab9b0d45903ac7a81b77
theraghav12 <khuranaraghav388@gmail.com>
2024-11-25 15:40:47 +0530

    added

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: "CLOUD_NAME",
+  api_key: "API_KEY",
+  api_secret: "API_SECRET",
 });
 
 const storage = new multer.memoryStorage();

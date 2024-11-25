0a9b451e27d7f0f7d71847bcd657393f867bdb0b
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-11-25 14:30:12 +0530

    chore: layout rollback

--- client/src/components/shopping-view/layout.jsx
+++ client/src/components/shopping-view/layout.jsx
@@ -3,7 +3,7 @@ import ShoppingHeader from "./header";
 
 function ShoppingLayout() {
   return (
-    <div className="flex flex-col bg-white overflow-hidden w-screen">
+    <div className="flex flex-col bg-white overflow-hidden">
       {/* common header */}
       <ShoppingHeader />
       <main className="flex flex-col w-full">

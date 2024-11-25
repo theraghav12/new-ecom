7dadb1e1f8a500e7408ac37725ec33560b412511
theraghav12 <khuranaraghav388@gmail.com>
2024-11-25 16:34:44 +0530

    added

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://new-ecom-beta.vercel.app/",
+    origin: "https://new-ecom-beta.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
